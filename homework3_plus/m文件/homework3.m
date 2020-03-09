07726cd88a352f86c506d680d40761807820e8fc
Maxin-C <57470650+Maxin-C@users.noreply.github.com>
2020-03-09 19:11:18 +0800

    insure the circle will plot in the right place

---
+++ homework3_plus/m文件/homework3.m
@@ -0,0 +1,31 @@
+% 声明一个1000*1000的矩形空间
+L=1000;
+S=ones(L);
+S(:,:)=255;
+S(1,:)=0;
+S(L,:)=0;
+S(:,1)=0;
+S(:,L)=0;
+
+Amp=L^2/4;% 放大倍数
+
+m=15;% 圆的数量
+count=0;
+for i=1:m
+    E=Edge(S,L);
+    [m,n]=find(E);
+    [x,y,r]=Circle(S,E);
+    for j=min(m):max(m)
+        for k=min(n):max(n)
+            if sqrt((j-x)^2+(k-y)^2)<=r
+                S(j,k)=0;
+            end
+        end
+    end
+%     figure;
+%     image(E)
+
+    count=count+r^2;
+end
+% count=count/Amp
+image(S);
